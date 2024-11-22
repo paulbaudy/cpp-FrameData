355fffd9c89030f8188e1f664d644e2da02400d8
Paul Baudy <paul.baudy@outlook.fr>
2024-11-22 02:41:42 +0100

    Ajoutez des fichiers projet.

---
+++ FrameData.cpp
@@ -0,0 +1,12 @@
+﻿// FrameData.cpp : définit le point d'entrée de l'application.
+//
+
+#include "FrameData.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
